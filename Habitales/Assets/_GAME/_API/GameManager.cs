6af70f5773a1a37ef1e663dad69bb321190fd68d
Justin Lu <justinlawrence.lu@benilde.edu.ph>
2026-02-18 15:03:55 +0800

    Midterm Build Unstable Cherry Pick

--- Habitales/Assets/_GAME/_API/GameManager.cs
+++ Habitales/Assets/_GAME/_API/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour {
     [SerializeField] private ActionManager actionManager;
     [SerializeField] private ZoneManager zoneManager;
     [SerializeField] private ResourceManager resourceManager;
+    [SerializeField] private RegionOutlineRenderer regionOutlineRenderer;
+
+
     
     [Header("Cascade Settings")]
     [SerializeField] [Range(0.05f, 0.5f)] private float diffusionRate = 0.15f;
@@ -110,6 +113,12 @@ public class GameManager : MonoBehaviour {
             actionManager.OnActionCompleted += HandleActionCompleted;
         }
         
+        if (regionOutlineRenderer == null)
+            regionOutlineRenderer = FindObjectOfType<RegionOutlineRenderer>();
+        if (regionOutlineRenderer == null)
+            Debug.LogError("RegionOutlineRenderer not found!");
+
+        
         if (showDebugInfo) {
             Debug.Log($"GameManager initialized | Systems: TileManager={tileManager != null}, TileSelector={tileSelector != null}, ActionUI={actionUI != null}, ActionManager={actionManager != null}, ZoneManager={zoneManager != null}");
         }
@@ -163,6 +172,8 @@ public class GameManager : MonoBehaviour {
             Debug.Log($"Tile selected: {tile.gridPosition} | Health: {tile.CalculateHealth():F1}%");
         }
         
+        regionOutlineRenderer?.ActivateRegion(tile.regionID); 
+        
         if (actionUI != null) {
             actionUI.ShowActionsForTile(tile, worldPosition);
         }
@@ -172,6 +183,8 @@ public class GameManager : MonoBehaviour {
         if (showDebugInfo) {
             Debug.Log("Tile deselected");
         }
+        
+        regionOutlineRenderer?.ClearRegion();
         
         if (actionUI != null) {
             actionUI.HideActions();
